e9e302fadd1088614aaf29a0cadd8cf3a05db355
ArianUlloa <arianlautaro06@gmail.com>
2026-06-30 18:29:33 -0300

    array y equipamiento

--- script.js
+++ script.js
@@ -1,5 +1,58 @@
+const equipamiento=[
+  "Guantes de Boxeo (12oz=65-70kg 14oz=75-80kg 16oz=+80kg)",
+  "Protector Bucal de silicona",
+  "Tibiales/espinilleras",
+  "Vendas para manos (5 metros minimo)",
+  "Pantalones cortos comodos",
+  "Remera manga corta comoda"
+];
+
+
 document.addEventListener("DOMContentLoaded", () =>{
    const formulario = document.getElementById("form-inscripcion");
+   const listaUl = document.getElementById("lista-equipamiento");
+   const inputBuscador = document.getElementById("buscador-equipamiento");
+   
+   function mostrarEquipamiento(listaFiltrada){
+      if (listaUl){
+         listaUl.innerHTML ="";
+         listaFiltrada.forEach(item => {
+            const li = document.createElement("li");
+            li.textContent = item;
+            li.style.padding = "8px 0";
+            li.style.fontSize ="16px";
+            listaUl.appendChild(li);
+         });
+      }
+   }
+    function filtrarContenido(textoBusqueda){
+      const resultado = equipamiento.filter(item =>
+        item.toLowerCase().includes(textoBusqueda.toLowerCase())
+      );
+      mostrarEquipamiento(resultado);
+    }
+   if(inputBuscador){
+      inputBuscador.addEventListener("input", (evento) =>{
+       filtrarContenido(evento.target.value);
+      })
+   }
+   if(listaUl){
+      listaUl.addEventListener("mouseover", (evento) =>{
+          if (evento.target.tagName === "LI"){
+             evento.target.style.color = "#d9534f";
+             evento.target.style.fontWeight = "bold";
+             evento.target.style.cursor = "pointer";
+          }
+      });
+      listaUl.addEventListener("mouseout", (evento) =>{
+        if(evento.target.tagName === "LI"){
+           evento.target.style.color = "";
+           evento.target.style.fontWeight = "";
+        }
+      })
+   }
+    mostrarEquipamiento(equipamiento);
+   
    function Vali_form (evento)
    {
       const nombre = document.getElementById("nombre").value;
